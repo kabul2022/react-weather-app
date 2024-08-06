bd0aa284950cb7bf5b827390b75456b3af56fe69
kabul2022 <aziza.rezaie45@gmail.com>
2024-08-06 12:48:26 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      Weather App
+      <footer>
+        This project was coded by Aziza Rezaie and is{" "}
+        <a
+          href="https://github.com/kabul2022/react-weather-app"
+          target="-blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
