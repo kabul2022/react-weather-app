88f18fa7a780e9773450a99576c71d756d984d25
kabul2022 <aziza.rezaie45@gmail.com>
2024-08-06 18:51:56 +0200

    Added the bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/kabul2022/" target="-blank">
-          Aziza Rezaie
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/kabul2022/react-weather-app"
-          target="-blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/kabul2022/" target="-blank">
+            Aziza Rezaie
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kabul2022/react-weather-app"
+            target="-blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
