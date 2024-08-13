ed0a28c781dd661c3fd2bcb727b26dc8a2927ae9
kabul2022 <aziza.rezaie45@gmail.com>
2024-08-13 22:21:42 +0200

    Update the files and make the uint changeable

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
-import WeatherIcon from "./WeatherIcon";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
@@ -13,21 +15,19 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="d-flex">
-            <WeatherIcon code={props.data.iconUrl} />
-
+          <div className="clearfix d-flex">
             <div className="float-left">
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">℃</span>
+              <WeatherIcon code={props.data.iconUrl} />
+            </div>
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity: {Math.round(props.data.humidity)}%</li>
-            <li>Wind: {Math.round(props.data.wind)} km/h</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
