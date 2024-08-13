ab363a2ada546e850daf76e492f7f3e2a0d39672
kabul2022 <aziza.rezaie45@gmail.com>
2024-08-13 14:14:59 +0200

    update  the icon file

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,29 +1,32 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
+
 export default function WeatherIcon(props) {
+  console.log(props);
   const codeMapping = {
-    "clear-sky-day": "sunny",
-    "clear-sky-night": "clear-night",
-    "few-clouds-day": "partlycloudy",
-    "few-clouds-night": "partlycloudy",
-    "scattered-clouds-day": "cloudy",
-    "scattered-clouds-night": "cloudy",
-    "broken-clouds-day": "cloudy",
-    "broken-clouds-night": "cloudy",
-    "shower-rain-day": "rainy",
-    "shower-rain-night": "rainy",
-    "rain-day": "pouring",
-    "rain-night": "pouring",
-    "thunderstorm-day": "lightning",
-    "thunderstorm-night": "lightning-rainy",
-    "snow-day": "snowy",
-    "snow-night": "snowy",
-    "mist-day": "fog",
-    "mist-night": "fog",
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "few-clouds-day": "PARTLY_CLOUDY_DAY",
+    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-day": "CLOUDY",
+    "scattered-clouds-night": "CLOUDY",
+    "broken-clouds-day": "CLOUDY",
+    "broken-clouds-night": "CLOUDY",
+    "shower-rain-day": "RAIN",
+    "shower-rain-night": "RAIN",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "thunderstorm-day": "SLEET",
+    "thunderstorm-night": "SLEET",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "FOG",
+    "mist-night": "FOG",
   };
+
   return (
     <ReactAnimatedWeather
-      iconUrl={codeMapping[props.code]}
+      icon={codeMapping[props.code]}
       color="#1e1e1e"
       size={52}
       animate={true}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,20 +13,21 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
+          <div className="d-flex">
+            <WeatherIcon code={props.data.iconUrl} />
+
             <div className="float-left">
-              <WeatherIcon code={props.data.iconUrl} />
+              <span className="temperature">
+                {Math.round(props.data.temperature)}
+              </span>
+              <span className="unit">℃</span>
             </div>
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">℃</span>
           </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>Humidity: {Math.round(props.data.humidity)}%</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
           </ul>
         </div>
       </div>
