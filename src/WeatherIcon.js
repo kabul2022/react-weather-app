f5204447d971914ca2d6c0585da5445c1dadbf8c
kabul2022 <aziza.rezaie45@gmail.com>
2024-09-13 10:21:20 +0200

    Changed  the app background color

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="blue"
+      color="1e1e1e"
       size={props.size}
       animate={true}
     />
