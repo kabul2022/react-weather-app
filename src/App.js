8d8ad80a0e28fd50112f92e111ea3c79f84a5b10
kabul2022 <aziza.rezaie45@gmail.com>
2024-09-13 09:41:45 +0200

    Refactored the code and fix the warnings

--- src/App.js
+++ src/App.js
@@ -9,22 +9,26 @@ export default function App() {
         <Weather defaultCity="New York" />
         <footer>
           This project was coded by{" "}
-          <a href="https://github.com/kabul2022/" target="-blank">
+          <a
+            href="https://github.com/kabul2022/"
+            target="-blank"
+            rel="noopener noreferrer"
+          >
             Aziza Rezaie
           </a>{" "}
           and is{" "}
           <a
             href="https://github.com/kabul2022/react-weather-app"
             target="-blank"
-            open-sourced
+            rel="noopener noreferrer"
           >
-            {" "}
-            on GitHub
+            open-sourced on GitHub
           </a>{" "}
           and hosted on{" "}
           <a
             href="https://master--silver-madeleine-9f0678.netlify.app/"
             target="-blank"
+            rel="noopener noreferrer"
           >
             Nitlify
           </a>
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,27 +6,42 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(false);
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
-  if (loaded) {
-    return (
-      <div className="Weatherforecast">
-        <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
-        </div>
-      </div>
-    );
-  } else {
+  function load() {
     let apiKey = "bb01a8cf18ea9o4532af5fe21atc5f3a";
     let longitude = props.coordinates.longitude;
     let latitude = props.coordinates.latitude;
 
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
+  }
+  if (loaded) {
+    return (
+      <div className="Weatherforecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
     return null;
   }
 }
